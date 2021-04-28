fd00e6d036b997510dcb8a9063eaa56e3038af94
Jorge Ou <8276144+Topomon@users.noreply.github.com>
2021-04-28 00:34:11 -0500

    First basic boilerplate

---
+++ snowpack.config.js
@@ -0,0 +1,24 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+		public: {url: '/', static: true},
+    src: {url: '/dist'},
+  },
+  plugins: [
+    "@snowpack/plugin-vue",
+		'@snowpack/plugin-postcss',
+		'@snowpack/plugin-webpack',
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-gray-400">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Snowpack + Vue 3"
+    };
+  }
+};
+</script>
